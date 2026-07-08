14d369767cf4011bd76639b2f13b882b2df83562
jamesfontanilla <jamesfontanilla.dev@proton.me>
2026-07-08 16:43:36 +0800

    fixed render crash

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,4 @@
 import "reflect-metadata";
-import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { apiEnv } from "./config/env";
@@ -8,13 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true
   });
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true
-    })
-  );
 
   app.enableCors({
     origin: apiEnv.corsOrigin
